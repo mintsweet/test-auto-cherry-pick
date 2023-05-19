6ed80b16f1bb4bffface94ab22d9ab64d0b88032
青湛 <0x1304570@gmail.com>
2023-05-19 07:52:02 +0000

    Merge 342b3a380a8660e4f7f05082ca94636eb4b4bbf1 into f8e26a69b970e964132cd5c94af8176a133f1a70

---
+++ .github/actions/auto-cherry-pick/cherry-pick.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TARGET_BRANCH=${LABEL_NAME##*-}
+PR_BRANCH="auto-cherry-pick-$TARGET_BRANCH-$GITHUB_SHA"
+
+echo "REPO_NAME: $REPO_NAME"
+echo "PR Number: $PR_NUMBER"
+echo "Label: $LABEL_NAME"
+echo "GitHub SHA: $GITHUB_SHA"
+echo "Author Email: $AUTHOR_EMAIL"
+echo "Author Name: $AUTHOR_NAME"
+echo "Assignees: $ASSIGNEES"
+echo "Target Branch: $TARGET_BRANCH"
+echo "PR Branch: $PR_BRANCH"
+
+git config --global user.email "$AUTHOR_EMAIL"
+git config --global user.name "$AUTHOR_NAME"
+
+git remote update
+git fetch --all
+git restore .
+git checkout -b $PR_BRANCH origin/$TARGET_BRANCH
+git cherry-pick $GITHUB_SHA
+git push origin $PR_BRANCH
+
+gh pr create -B $TARGET_BRANCH -H $PR_BRANCH -t "cherry-pick: $PR_NUMBER" -b "this a auto create pr!<br />cherry pick from https://github.com/$REPO_NAME/pull/$PR_NUMBER" -a $ASSIGNEES
+# gh pr comment $PR_NUMBER --body "🤖 cherry pick finished successfully 🎉!<br/>See: https://github.com/$REPO_NAME/pull/1"
