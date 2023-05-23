cc09fa39587c59d7ae36f5edca7743ea51c9e457
mintsweet <0x1304570@gmail.com>
2023-05-23 12:37:31 +0800

    feat: add error handle for cherry pick

--- .github/actions/auto-cherry-pick/cherry-pick.sh
+++ .github/actions/auto-cherry-pick/cherry-pick.sh
@@ -3,6 +3,7 @@
 TARGET_BRANCH=${LABEL_NAME##*-}
 PR_BRANCH="auto-cherry-pick-$TARGET_BRANCH-$GITHUB_SHA"
 
+echo "==================== Basic Info ===================="
 echo "REPOSITORY: $REPOSITORY"
 echo "PR Number: $PR_NUMBER"
 echo "PR Title: $PR_TITLE"
@@ -15,15 +16,23 @@ echo "Assignees: $ASSIGNEES"
 echo "Target Branch: $TARGET_BRANCH"
 echo "PR Branch: $PR_BRANCH"
 
+
+echo "==================== Git Configuration ===================="
 git config --global user.email "$AUTHOR_EMAIL"
 git config --global user.name "$AUTHOR_NAME"
 
+
+echo "==================== Git Cherry Pick ===================="
 git remote update
 git fetch --all
 git restore .
 git checkout -b $PR_BRANCH origin/$TARGET_BRANCH
-git cherry-pick $GITHUB_SHA
+git cherry-pick $GITHUB_SHA &> (
+	gh pr comment $PR_NUMBER --body "🤖 The current file has a conflict, and the pr cannot be automatically created."
+	exit 1
+)
 git push origin $PR_BRANCH
 
+echo "==================== GitHub Auto Create PR ===================="
 gh pr create -B $TARGET_BRANCH -H $PR_BRANCH -t "cherry-picked-from: #$PR_NUMBER $PR_TITLE" -b "this a auto create pr!<br />cherry pick from https://github.com/$REPOSITORY/pull/$PR_NUMBER<br />$PR_BODY" -a $ASSIGNEES
 gh pr comment $PR_NUMBER --body "🤖 cherry pick finished successfully 🎉!"
