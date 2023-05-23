61d5c1462f9dbc1173c075160fec92d63c10c10b
mintsweet <0x1304570@gmail.com>
2023-05-23 12:46:52 +0800

    fix: the error for cherry-pick failed

--- .github/actions/auto-cherry-pick/cherry-pick.sh
+++ .github/actions/auto-cherry-pick/cherry-pick.sh
@@ -16,18 +16,16 @@ echo "Assignees: $ASSIGNEES"
 echo "Target Branch: $TARGET_BRANCH"
 echo "PR Branch: $PR_BRANCH"
 
-
 echo "==================== Git Configuration ===================="
 git config --global user.email "$AUTHOR_EMAIL"
 git config --global user.name "$AUTHOR_NAME"
-
 
 echo "==================== Git Cherry Pick ===================="
 git remote update
 git fetch --all
 git restore .
 git checkout -b $PR_BRANCH origin/$TARGET_BRANCH
-git cherry-pick $GITHUB_SHA &> (
+git cherry-pick $GITHUB_SHA || (
 	gh pr comment $PR_NUMBER --body "🤖 The current file has a conflict, and the pr cannot be automatically created."
 	exit 1
 )
