fc9caf8b9326360625219c07fc869ca69518e577
mintsweet <0x1304570@gmail.com>
2023-05-23 12:22:51 +0800

    feat: improve the title and body in auto create pr

--- .github/actions/auto-cherry-pick/cherry-pick.sh
+++ .github/actions/auto-cherry-pick/cherry-pick.sh
@@ -5,6 +5,8 @@ PR_BRANCH="auto-cherry-pick-$TARGET_BRANCH-$GITHUB_SHA"
 
 echo "REPOSITORY: $REPOSITORY"
 echo "PR Number: $PR_NUMBER"
+echo "PR Title: $PR_TITLE"
+echo "PR Body: $PR_BODY"
 echo "Label: $LABEL_NAME"
 echo "GitHub SHA: $GITHUB_SHA"
 echo "Author Email: $AUTHOR_EMAIL"
@@ -23,5 +25,5 @@ git checkout -b $PR_BRANCH origin/$TARGET_BRANCH
 git cherry-pick $GITHUB_SHA
 git push origin $PR_BRANCH
 
-gh pr create -B $TARGET_BRANCH -H $PR_BRANCH -t "cherry-pick: $PR_NUMBER" -b "this a auto create pr!<br />cherry pick from https://github.com/$REPOSITORY/pull/$PR_NUMBER" -a $ASSIGNEES
+gh pr create -B $TARGET_BRANCH -H $PR_BRANCH -t "cherry-picked-from: #$PR_NUMBER $PR_TITLE" -b "this a auto create pr!<br />cherry pick from https://github.com/$REPOSITORY/pull/$PR_NUMBER<br />$PR_BODY" -a $ASSIGNEES
 gh pr comment $PR_NUMBER --body "🤖 cherry pick finished successfully 🎉!"
